54b54d8d795d7c197e54ff6dff6a80ec66ce429f
uynilo9 <uynilo9@outlook.com>
2025-12-31 01:39:25 +0800

    refactor(AtCoder): refine the solution of keyence 2019 task B
    Use a clearer and more straightforward method.

--- atcoder/atcoder_keyence2019_b.cpp
+++ atcoder/atcoder_keyence2019_b.cpp
@@ -5,28 +5,16 @@ int main() {
     string input;
     cin>>input;
 
-    string keyence="keyence";
+    const string keyence="keyence";
+    for (int i=0;i<=7;i++) {
+        if (input.substr(0,i)+input.substr(input.size()-(7-i))==keyence) {
+            cout<<"YES"<<"\n";
 
-    int char_index=-1;
-    int substr_count=0;
-    for (const auto ch:input) {
-        if (ch==keyence[char_index+1])
-            char_index++;
-        else {
-            substr_count++;
-            break;
+            return 0;
         }
     }
 
-    if (substr_count==0) {
-        cout<<"YES"<<"\n";
-        return 0;
-    }
-
-    input=input.substr(char_index+1);
-    keyence=keyence.substr(char_index+1);
-
-    cout<<(input.substr(input.size()-keyence.size())==keyence?"YES":"NO")<<"\n";
+    cout<<"NO"<<"\n";
 
     return 0;
 }
